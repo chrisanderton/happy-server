b547a2cb3c6b3b631598fe7a2a4b0c19b791ae3a
Chris Anderton <chrisanderton@users.noreply.github.com>
2025-08-30 18:35:11 +0100

    Use environment variable for S3 SSL configuration

--- sources/storage/files.ts
+++ sources/storage/files.ts
@@ -2,7 +2,7 @@ import * as Minio from 'minio';
 
 export const s3client = new Minio.Client({
     endPoint: process.env.S3_HOST!,
-    useSSL: true,
+    useSSL: process.env.S3_USE_SSL!,
     accessKey: process.env.S3_ACCESS_KEY!,
     secretKey: process.env.S3_SECRET_KEY!,
 });
@@ -12,6 +12,8 @@ export const s3bucket = process.env.S3_BUCKET!;
 export const s3host = process.env.S3_HOST!
 
 export const s3public = process.env.S3_PUBLIC_URL!;
+
+export const s3ssl = process.env.S3_USE_SSL!;
 
 export async function loadFiles() {
     await s3client.bucketExists(s3bucket); // Throws if bucket does not exist or is not accessible
